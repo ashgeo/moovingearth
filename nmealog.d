82690900cfbe0cfb16d88ca3fde89f53b75d98ef
bartdb <bartdb@daf12d2a-a219-0410-9b4a-d5df6cb5a060>
2006-09-26 13:06:10 +0000

    Forgot to add nmealog

---
+++ nmealog.d
@@ -0,0 +1,28 @@
+﻿module nmealog;
+
+private import std.stdio;
+
+private import nmeahub;
+
+class nmeaLogger
+{
+	private char[] outfile;
+	this( char[] folder, char[] filen ) {
+		outfile = folder ~ filen;
+
+		// OK, ready for data
+		synchronized( nmeaHub )
+			nmeaHub.nmeaProcessors ~= &onNMEA;
+	}
+
+	void onNMEA( ubyte[] data ) {
+		try {
+			std.file.append( outfile, data );
+		}
+		catch
+		{
+			writefln( "Could not log NMEA" );
+		}
+	}
+
+}
